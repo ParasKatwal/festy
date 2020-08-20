2e73e3ef8d4a9dba98d95b9a19f0202985570261
ParasKatwal <katwal.paras@gmail.com>
2020-08-20 22:36:45 +0545

    first commit

---
+++ src/components/NavBar.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="navBar">
+    <v-container>
+      <div class="d-flex justify-space-between">
+        <h1>LOGO</h1>
+        <v-btn target="_blank" text>
+          <v-icon>mdi-open-in-new</v-icon>
+          <span class="mr-2">המועדפים שלי</span>
+        </v-btn>
+      </div>
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NavBar"
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
